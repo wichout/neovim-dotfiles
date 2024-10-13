bb1e4266a3b195fece51a6f28988db665dba1814
Luis Adolfo Taddei Gonzalez <luisadolfotaddeigonzalez@gmail.com>
2024-10-12 22:42:15 -0700

    Makin it modular

---
+++ lua/lazy-plugins.lua
@@ -0,0 +1,70 @@
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+require('lazy').setup({
+  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  -- NOTE: Plugins can also be added by using a table,
+  -- with the first argument being the link and the following
+  -- keys can be used to configure plugin behavior/loading/etc.
+  --
+  -- Use `opts = {}` to force a plugin to be loaded.
+  --
+  -- NOTE: Plugins can specify dependencies.
+  --
+  -- The dependencies are proper plugin specifications as well - anything
+  -- you do for a plugin at the top level, you can do for a dependency.
+  --
+  -- Use the `dependencies` key to specify the dependencies of a particular plugin
+
+  require 'wichout.plugins.autopairs',
+  require 'wichout.plugins.cmp',
+  require 'wichout.plugins.color-scheme',
+  require 'wichout.plugins.conform',
+  -- require 'wichout.plugins.debug',
+  require 'wichout.plugins.gitsigns',
+  require 'wichout.plugins.indent_line',
+  require 'wichout.plugins.mini',
+  -- require 'kickstart.plugins.neo-tree',
+  -- require 'kickstart.plugins.lint',
+  require 'wichout.plugins.lsp-config',
+  require 'wichout.plugins.telescope',
+  require 'wichout.plugins.treesitter',
+  require 'wichout.plugins.todo-comments',
+  require 'wichout.plugins.which-key',
+
+  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
+  { import = 'custom.plugins' },
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+-- vim: ts=2 sts=2 sw=2 et
