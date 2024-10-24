329ef899f12bad6c63523de63d203f8224442734
Luis Adolfo Taddei Gonzalez <luisadolfotaddeigonzalez@gmail.com>
2024-10-24 12:48:56 -0700

    added symbols for all gitsigns

--- lua/wichout/plugins/gitsigns.lua
+++ lua/wichout/plugins/gitsigns.lua
@@ -13,11 +13,19 @@ return {
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
+        add = { text = '│' },
+        change = { text = '│' },
+        delete = { text = '│' },
+        topdelete = { text = '│' },
+        changedelete = { text = '│' },
+      },
+      signs_staged = {
+        add = { text = '│' },
+        change = { text = '│' },
+        delete = { text = '│' },
+        topdelete = { text = '│' },
+        changedelete = { text = '│' },
+        untracked = { text = '│' },
       },
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
